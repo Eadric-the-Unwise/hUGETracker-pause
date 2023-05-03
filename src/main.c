d91043ae1f5f5641c3c7703facfd4f2d12667e8a
Eadric-the-Unwise <42740694+Eadric-the-Unwise@users.noreply.github.com>
2023-05-02 19:03:21 -0700

    EOD

--- src/main.c
+++ src/main.c
@@ -1,4 +1,7 @@
 #include <gb/gb.h>
+
+#include "../sound/musicmanager.h"
+#include "../sound/sfxplayer.h"
 #include "hUGEDriver.h"
 
 extern const hUGESong_t sample_song;
@@ -6,13 +9,14 @@ extern const hUGESong_t sample_song;
 UBYTE joy;
 UBYTE c0 = 0, c1 = 0, c2 = 0, c3 = 0;
 
-const unsigned char pattern1[] = {0x80,0x80,0x40,0x40,0x20,0x20,0x10,0x10,0x08,0x08,0x04,0x04,0x02,0x02,0x01,0x01};
-const unsigned char pattern2[] = {0x00,0x00,0x7E,0x7E,0x40,0x40,0x54,0x54,0x48,0x48,0x54,0x54,0x40,0x40,0x00,0x00};
-const unsigned char map[]      = {0x00,0x20};
+const unsigned char pattern1[] = {0x80, 0x80, 0x40, 0x40, 0x20, 0x20, 0x10, 0x10, 0x08, 0x08, 0x04, 0x04, 0x02, 0x02, 0x01, 0x01};
+const unsigned char pattern2[] = {0x00, 0x00, 0x7E, 0x7E, 0x40, 0x40, 0x54, 0x54, 0x48, 0x48, 0x54, 0x54, 0x40, 0x40, 0x00, 0x00};
+const unsigned char map[] = {0x00, 0x20};
 
 void main() {
+    static uint8_t music_paused = FALSE;
     LCDC_REG = 0xD1;
-    BGP_REG  = 0b11100100;
+    BGP_REG = 0b11100100;
     NR52_REG = 0x80;
     NR51_REG = 0xFF;
     NR50_REG = 0x77;
@@ -24,15 +28,35 @@ void main() {
         hUGE_init(&sample_song);
         add_VBL(hUGE_dosound);
     }
-        
-    while(1) {
+    // music_stop(), music_pause(music_paused = FALSE);
+    while (1) {
         wait_vbl_done();
         joy = joypad();
         switch (joy) {
-            case J_UP    : c0 ^= 1; hUGE_mute_channel(HT_CH1, c0); set_bkg_tiles(0,0,1,1,&map[c0 & 1]); waitpadup(); break;
-            case J_DOWN  : c1 ^= 1; hUGE_mute_channel(HT_CH2, c1); set_bkg_tiles(1,0,1,1,&map[c1 & 1]); waitpadup(); break;
-            case J_LEFT  : c2 ^= 1; hUGE_mute_channel(HT_CH3, c2); set_bkg_tiles(2,0,1,1,&map[c2 & 1]); waitpadup(); break;
-            case J_RIGHT : c3 ^= 1; hUGE_mute_channel(HT_CH4, c3); set_bkg_tiles(3,0,1,1,&map[c3 & 1]); waitpadup(); break;
+            case J_UP:
+                c0 ^= 1;
+                hUGE_mute_channel(HT_CH1, c0);
+                set_bkg_tiles(0, 0, 1, 1, &map[c0 & 1]);
+                waitpadup();
+                break;
+            case J_DOWN:
+                c1 ^= 1;
+                hUGE_mute_channel(HT_CH2, c1);
+                set_bkg_tiles(1, 0, 1, 1, &map[c1 & 1]);
+                waitpadup();
+                break;
+            case J_LEFT:
+                c2 ^= 1;
+                hUGE_mute_channel(HT_CH3, c2);
+                set_bkg_tiles(2, 0, 1, 1, &map[c2 & 1]);
+                waitpadup();
+                break;
+            case J_RIGHT:
+                c3 ^= 1;
+                hUGE_mute_channel(HT_CH4, c3);
+                set_bkg_tiles(3, 0, 1, 1, &map[c3 & 1]);
+                waitpadup();
+                break;
         }
     }
 }
